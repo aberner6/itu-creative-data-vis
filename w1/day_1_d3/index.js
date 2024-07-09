1516be45fdef74b43085fbae26cc5bd12012c973
Annelie <100210244+kgl-test-user@users.noreply.github.com>
2024-07-09 10:29:11 +0200

    updates from live coding

--- w1/day_1_d3/index.js
+++ w1/day_1_d3/index.js
@@ -1,22 +1,82 @@
 //GOAL: can we set up a local coding environment, webpage and draw 1 shape on it?
 
-var width = 800;
-var height = 500;
-var xPos = width/2;
-var yPos = height/2;
-var rad = 10;
-
+var w = 500;
+var h = 500;
 
 var canvas = d3.select("#myVis")
-                .append("svg")
-                .attr("width", width)
-                .attr("height", height);
+       .append("svg")
+       .attr("width",w)
+       .attr("height",h)
+       .style("background-color","blue");
 
- var circleFromD3 = canvas.append("circle")
-        .attr("cx", xPos)
-        .attr("cy", yPos)
-        .attr("r", rad)
-        .attr("fill","red");
+canvas.append("circle")
+       .attr("cx", w/2)
+       .attr("cy", h/2)
+       .attr("r", 50)
+       .attr("fill","white")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var width = 800;
+// var height = 500;
+// var xPos = width/2;
+// var yPos = height/2;
+// var rad = 10;
+
+
+// var canvas = d3.select("#myVis")
+//                 .append("svg")
+//                 .attr("width", width)
+//                 .attr("height", height);
+
+//  var circleFromD3 = canvas.append("circle")
+//         .attr("cx", xPos)
+//         .attr("cy", yPos)
+//         .attr("r", rad)
+//         .attr("fill","red");
 
 
 
