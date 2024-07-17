46077e35ea138a0678458b55172eb9f70db39b91
Annelie <100210244+kgl-test-user@users.noreply.github.com>
2024-07-17 11:58:30 +0200

    final update from the class live coding

--- w2/day_7_rot/index_4.js
+++ w2/day_7_rot/index_4.js
@@ -2,12 +2,16 @@
 //drive shapes rotation based on data
 //look into using grouping to organise and move elements around the canvas
 
+
+//set up
 var w = 1000;
 var h = 800;
 var canvas = d3.select("#canvas").append("svg")
 	.attr("width", w)
 	.attr("height", h)
 	.style("background-color","black");
+
+//loading data
 var skyData = [];
 d3.json("skyData.json").then(function(data){
 	skyData = data;
@@ -15,12 +19,13 @@ d3.json("skyData.json").then(function(data){
 	draw();
 })
 
-//draw a circle per data item
+//drawing
 function draw(){
 	console.log(skyData)
 	var r = 10;
-	var margin = r*2;
+	var margin = r*4;
 	
+	//set up mins and maxes of cloud cover
 	var cloudCov = skyData.map(function(d){
 		return d.cloudCov
 	})
@@ -28,17 +33,68 @@ function draw(){
 	var max = d3.max(cloudCov);
 
 	console.log(min+" "+max)
-	
-	var xAxis = d3.scaleLinear()
-					.domain([0, skyData.length])
-					.range([margin, w-margin])
-	var circles = canvas.selectAll("circle")
-					.data(skyData)
-					.join("circle")
-					.attr("cx",function(d,i){
-						return xAxis(i);
-					})
-					.attr("cy",h/2)
-					.attr("r",r)
-					.attr("fill","white")
+	//pulling array of days for use in the xscale
+	var days = skyData.map(function(d){
+		return d.day;
+	})
+	console.log(days)
+	//x scale that should position items left to right
+	//according to weekday
+	var xAxis = d3.scaleBand()
+					.domain(days)
+					.range([margin, w-margin]);
+	//y scale that should separate items into rows 
+	//when they are from the following week
+	var yScale = d3.scaleLinear()
+					.domain([0,6])
+					.range([h/4, h/2])
+	console.log(xAxis("sunday"))
+
+	//preparing spaces on the canvas that are in the x y positions
+	var gElements = canvas.selectAll("g")
+							.data(skyData)
+							.join("g")
+							.attr("transform",function(d,i){
+								return "translate("+xAxis(d.day)+","+yScale(Math.floor(i/7))+")"
+							})
+	//on top of the g elements, we are adding a circle
+	gElements.append("circle")
+				.attr("cx",0)
+				.attr("cy",0)
+				.attr("r",r)
+				.attr("fill","white");
+
+
+	//make a scale that rotates the lines according to how much cloud cover is observed
+	var rotationScale = d3.scaleLinear()
+							.domain([min, max])
+							.range([90, 270])
+	//on top of the g elements, let's add a line
+	//and let's rotate that line according to cloud cover data
+	gElements.append("rect")
+				.attr("width",2)
+				.attr("height",r)
+				.attr("x",0)
+				.attr("y",0)
+				.attr("transform",function(d){
+					return "rotate("+rotationScale(d.cloudCov)+")"
+				})
+
+
+	//on top of those spaces we will draw circles and other shapes						
+// 	var circles = canvas.selectAll("circle")
+// 					.data(skyData)
+// 					.join("circle")
+// 					.attr("class", function(d){
+// 						return d.day
+// 					})
+// 					.attr("cx",function(d){
+// 						return xAxis(d.day);
+// 					})
+// 					.attr("cy",function(d,i){
+// 						console.log(Math.floor(i/7))
+// 						return yScale(Math.floor(i/7))
+// 					})
+// 					.attr("r",r)
+// 					.attr("fill","white")
 }
