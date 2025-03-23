480a42cedf7b8c2038308e07fcf664bb7759b76e
Annelie <100210244+kgl-test-user@users.noreply.github.com>
2025-03-23 14:21:12 +0100

    double checked and clarified

--- s3/day_9_transitions_triggers/index.js
+++ s3/day_9_transitions_triggers/index.js
@@ -1,6 +1,6 @@
 var w = 1000;
 var h = 1000;
-var r = 20;
+var r = 10;
 
 var svg = d3.select("#canvas")
   .append("svg")
@@ -13,81 +13,89 @@ var svg = d3.select("#canvas")
 	updateData();
   })
 
-  // ////1. CREATE CIRCLES ACCORDING TO DATA
-//   var arrData = [5, 8, 6, 10, 7, 2]
-//   var circs = svg.selectAll('anything')
-// 	.data(arrData)
-// 	.join('circle')
-// 	.attr('cx', w/2)
-// 	.attr('cy', h/2)
-// 	.attr("r", 1)
-// 	.attr('fill','none')
-// 	.attr("stroke","pink")
-// 	.attr("stroke-width",5)
-// 	.transition()
-// 	.duration(8000)
-// 	.attr('r', function(d){
-// 		return r*d;
-// 	})
-// 	.attr("stroke","blue")
+  //// CREATE CIRCLES ACCORDING TO DATA
+  var arrData = [5, 8, 20, 9, 4, 2]
+  var circs = svg.selectAll('anything')
+	.data(arrData)
+	.join('circle')
+	.attr('cx', w/2)
+	.attr('cy', h/2)
+	.attr("r", 1)
+	.attr('fill','none')
+	.attr("stroke","pink")
+	.attr("stroke-width",5)
+	.transition()
+	.duration(2000)
+	.attr('r', function(d){
+		return r*d;
+	})
+	.attr("stroke","blue")
 
 
-////THEN UPDATE THEM ALL INTO NEW DATA
+//// 1. comment me in 
+//// THEN UPDATE THEM ALL INTO NEW DATA
+//// NOTE THAT THE NEW VISUALISATION WILL ONLY SHOW 5 CIRCLES (5 DATA POINTS)
 //   var newData = [];
 //   function updateData(){
-// 	newData = [7,10,3,6,3]
+// 	newData = [7,10,3,6,3,12]
 // 	updateVis();
 //   }
 
 //   function updateVis(){
-// 	  console.log("hi")
-// 	d3.selectAll("circle").exit().remove();
-// 	circs
+// 	// note that if you do not add the chained command of .remove() after the .exit(), 
+// 	// the 6th data element will remain (it has not been replaced)
+// 	// .remove();
+// 	d3.selectAll("circle").exit(); 
+
+// 	d3.selectAll("circle")
 // 		.data(newData)
 // 		.transition()
 // 		// .delay(function(d, i) {
-// 		// 	return i / newData.length * 4000;   // <-- Where the magic happens
+// 		// 	return i / newData.length * 4000;   // adding this in will make each data piece arrive in a timed entrance
 // 		// })
 // 		.duration(2000)
 // 		.attr('r', function(d){
 // 			return r*d;
 // 		})
+// 		.attr("stroke-width",1)
+// 		.attr('stroke','pink')
 //   }
 
 
 
 
 
-////2. TRANSITIONS WITH PROGRESSIVELY INCREASING DATA
-var arrData = [5, 8, 6, 10, 7, 2]
-var counter = 0;
-var newData = [];
-var circs;
-function updateData(){
-	newData.push(arrData[counter]);
-	counter++;
-	updateVis();
-}
+//// 2. comment me in next (and section 1 comment out)
+//// TRANSITIONS WITH PROGRESSIVELY MORE DATA
+// var arrData = [5, 8, 6, 10, 7, 2]
+// var counter = 0;
+// var newData = [];
+// var circs;
+// function updateData(){
+// 	newData.push(arrData[counter]);
+// 	counter++;
+// 	updateVis();
+// }
 
-function updateVis(){
-	console.log("hi")
+// function updateVis(){
+// 	console.log("hi")
 	
-	circs = svg.selectAll('circle.newData')
-	.data(newData)
-	.join('circle')
-	.attr("class","newData")
-	.attr('cx', w/2)
-	.attr('cy', h/2)
-	.attr("class","newData")
-	.attr('fill','none')
-	.attr("stroke","pink")
-	.transition()
-	.ease(d3.easeExpInOut)
-	.duration(2000)
-	.attr('r', function(d){
-		return d*r;
-	})
-}
+// 	circs = svg.selectAll('circle.newData')
+// 	.data(newData)
+// 	.join('circle')
+// 	.attr("class","newData")
+// 	.attr('cx', w/2)
+// 	.attr('cy', h/2)
+// 	.attr("class","newData")
+// 	.attr('fill','none')
+// 	.attr("stroke","pink")
+// 	.transition()
+// 	.ease(d3.easeExpInOut)
+// 	.duration(2000)
+// 	.attr('r', function(d){
+// 		return d*r;
+// 	})
+// }
 
 // function animateCircle(){
 // 	circs.transition()
@@ -100,68 +108,69 @@ function updateVis(){
 
 
 
-////3. CHANGING DATA
+////3. comment me in next (and section 2 comment out) 
+//// CHANGING DATA
 // var data = [];
 // svg.on("click", function(){
 // 	updateData();
-// 	updateVis();
 // })
 // function updateData() {
 //   data = []; //empty the data
-//   var howLong = Math.random()*10;
+//   var howLong = Math.random()*100;
 //   for(var i=0; i<howLong; i++) {
 //     data.push(Math.random()*w);
 //   }
+//   updateVis(data);
 // }
-// function updateVis() {
-//   d3.select('svg')
-//     .selectAll('circle')
-//     .data(data)
-//     .join('circle')
-//     .attr('cy', h/2)
-//     .attr('r', r)
-//     .attr('fill','white')
-//     .attr('stroke','pink')
-//     .transition()
-// 		.duration(4000)
-//     .attr('cx', function(d) {
-//       return d;
-//     });
+// function updateVis(data) {
+// 	var xScale = d3.scaleLinear().domain(d3.extent(data)).range([r, w-r])
+
+// 	d3.selectAll("circle").exit().remove(); 
+// 	d3.selectAll('circle')
+// 		.data(data)
+// 		.attr('cy', h/2)
+// 		.attr('r', r)
+// 		.attr('fill','white')
+// 		.attr('stroke','pink')
+// 		.transition()
+// 		.duration(3000)
+// 		.attr('cx', function(d) {
+// 			return xScale(d);
+// 		});
 // }
 
 
-////4. CHUNKS OF DATA
-// var wholeData = [1,3,5,10,13,15,20,23,25];
-// var partData = [];
-// var index = 0;
+//// 4. comment me in next (and section 3 comment out) 
+/// ADD CHUNKS OF DATA to vis
+var wholeData = [1,3,5,10,13,15,20,23,25];
+var partData = [];
+var index = 0;
 
-// svg.on("click", function(){
-// 	updateData();
-// })
-// var index = 0;
-// var inc = 2;
-// partData = []; 
-// function updateData() {
-// 	for(var i=index; i<index+inc; i++) {
-// 		partData.push(wholeData[i]);
-// 	}
-// 	console.log(partData)
-// 	index = index+inc;
-// 	updateVis();
-// }
-// function updateVis() {
-//   d3.select('svg')
-//     .selectAll('circle')
-//     .data(partData)
-//     .join('circle')
-//     .attr('cy', h/2)
-// 	.attr('cx', w/2)
-//     .attr('r', 0)
-//     .attr('fill','none')
-// 	.attr('stroke','white')
-//     .transition()
-//     .duration(2000)
-//     .attr('r', function(d) {
-//       return d*10;
-//     });
-// }
+svg.on("click", function(){
+	updateData();
+})
+var index = 0;
+var inc = 2;
+partData = []; 
+function updateData() {
+	for(var i=index; i<index+inc; i++) {
+		partData.push(wholeData[i]);
+	}
+	console.log(partData)
+	index = index+inc;
+	updateVis();
+}
+function updateVis() {
+	d3.selectAll("circle").exit().remove(); 
+	var addCircs = svg.selectAll('.add')
+		.data(partData)
+		.join('circle')
+		.attr('class','add')
+		.attr('cy', h/2)
+		.attr('cx', w/2)
+		.attr('fill','none')
+		.attr('stroke','white')
+		.attr('r', function(d) {
+		return d*10;
+		});
+}
